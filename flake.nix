a3dc319497a484b1372c13d1cbfa441170cf9098
Szabo Istvan <pisti4395@gmail.com>
2026-06-09 18:15:03 +0200

    dasddsasdasdfdsasafdsafdfdsa

--- flake.nix
+++ flake.nix
@@ -1,57 +1,113 @@
+# SPDX-License-Identifier: Unlicense
 {
-  description = "Rust ai assistant";
-
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    # systems.url = "github:nix-systems/default";
+
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+
     rust-overlay.url = "github:oxalica/rust-overlay";
+    crane.url = "github:ipetkov/crane";
   };
 
+  # Add settings for your binary cache.
+  # nixConfig = {
+  #   extra-substituters = [
+  #   ];
+  #   extra-trusted-public-keys = [
+  #   ];
+  # };
+
   outputs =
-    {
-      self,
-      nixpkgs,
-      rust-overlay,
-    }:
+    inputs@{ nixpkgs, flake-parts, ... }:
     let
-      system = "x86_64-linux";
-      overlays = [ (import rust-overlay) ];
-      pkgs = import nixpkgs { inherit system overlays; };
-
-      runtimeDeps = with pkgs; [
-        alsa-lib
-        fontconfig
-      ];
-      buildDeps = with pkgs; [
-        pkg-config
-        (rust-bin.stable.latest.default.override {
-          extensions = [ "rust-src" ];
-        })
-      ];
+      # For details on these options, See
+      # https://github.com/oxalica/rust-overlay?tab=readme-ov-file#cheat-sheet-common-usage-of-rust-bin
+      #
+      # Channel of the Rust toolchain (stable or beta).
+      rustChannel = "stable";
+      # Version (latest or specific date/semantic version)
+      rustVersion = "latest";
+      # Profile (default or minimal)
+      rustProfile = "default";
     in
-    {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = buildDeps ++ runtimeDeps;
-      };
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
 
-      packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
-        pname = "raa";
-        version = "0.1.0";
-        src = ./.;
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
 
-        cargoLock = {
-          lockFile = ./Cargo.lock;
+      perSystem =
+        {
+          config,
+          system,
+          pkgs,
+          lib,
+          craneLib,
+          commonArgs,
+          ...
+        }:
+        {
+          _module.args = {
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [ inputs.rust-overlay.overlays.default ];
+            };
+            craneLib = (inputs.crane.mkLib pkgs).overrideToolchain (
+              pkgs: pkgs.rust-bin.${rustChannel}.${rustVersion}.${rustProfile}
+            );
+            commonArgs = {
+              # Depending on your code base, you may have to customize the
+              # source filtering to include non-standard files during the build.
+              # See
+              # https://crane.dev/source-filtering.html?highlight=source#source-filtering
+              src = craneLib.cleanCargoSource (craneLib.path ./.);
+
+              # nativeBuildInputs = with pkgs; [
+              #   pkg-config
+              # ];
+
+              # buildInputs = with pkgs; [
+              #   openssl
+              # ];
+            };
+          };
+
+          # Build the executable package.
+          packages.default = craneLib.buildPackage (
+            commonArgs
+            // {
+              cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+            }
+          );
+
+          devShells.default = craneLib.devShell {
+            inputsFrom = [
+              config.packages.default
+            ];
+
+            packages = [
+              pkgs.rust-analyzer-unwrapped
+              # If your repository is a polyglot project, add other dependencies
+              # such as corepack, typescript-go, etc.
+            ];
+
+            RUST_SRC_PATH = "${
+              pkgs.rust-bin.${rustChannel}.${rustVersion}.rust-src
+            }/lib/rustlib/src/rust/library";
+          };
+
+          treefmt = {
+            projectRootFile = "Cargo.toml";
+            programs = {
+              actionlint.enable = true;
+              nixfmt.enable = true;
+              rustfmt.enable = true;
+              zizmor.enable = true;
+            };
+          };
         };
-
-        nativeBuildInputs = [ pkgs.pkg-config ];
-        buildInputs = runtimeDeps;
-
-        doCheck = false;
-      };
-
-      apps.${system}.default = {
-        type = "app";
-        program = "${self.packages.${system}.default}/bin/raa";
-      };
     };
 }
-
